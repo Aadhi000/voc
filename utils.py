1a395361d294c4528a7f9ca447fec22d153e9a44
ααdhí | αrjun vr <87893573+Aadhi000@users.noreply.github.com>
2022-07-03 21:12:29 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -375,3 +375,23 @@ def humanbytes(size):
         size /= power
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
+
+
+async def get_shortlink(link):
+    https = link.split(":")[0]
+    if "http" == https:
+        https = "https"
+        link = link.replace("http", https)
+    url = f'https://du-link.in/api'
+    params = {'api': '453bae6d750e7ecdb5c3c0aa72d57bef671d0ecc',
+              'url': link,
+              }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+            data = await response.json()
+            if data["status"] == "success":
+                return data['shortenedUrl']
+            else:
+                return f"Error: {data['message']}"
