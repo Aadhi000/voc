6ac7b4a849022910181f95b6faf6e13effd05dad
ααdhí <87893573+Aadhi000@users.noreply.github.com>
2022-08-31 13:20:46 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -384,7 +384,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://api.shareus.in/shortLink?token=RynKaLU9OlVu82ThC0veYrUwDuL2'
+    url = f'https://api.shareus.in/shortLink'
     params = {'api': 'RynKaLU9OlVu82ThC0veYrUwDuL2',
               'url': link,
               }
