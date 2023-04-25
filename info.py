f0e420f7ce7b2a95b41f970f07aad27ea8c49fca
ααdhí <87893573+Aadhi000@users.noreply.github.com>
2023-04-25 19:27:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
 DELETE_TIME = int(environ.get('DELETE_TIME', 1800))
-CH_LINK = environ.get('CH_LINK' 0)
+CH_LINK = environ.get('CH_LINK', 0)
 CH_FILTER = int(environ.get('CH_FILTER', 0))
 
 
